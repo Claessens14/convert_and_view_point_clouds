d364fb0f832151ef19f6077b2e2f295514f3c00c
Jacob Claessens <jacobwclaessens@gmail.com>
2023-02-26 11:23:42 -0500

    image to point cloud semantics is now functional

--- img_to_ptcld.py
+++ img_to_ptcld.py
@@ -6,14 +6,14 @@ import open3d as o3d
 
 observations = torch.load("train_observations_lst.pt")
 
-print(observations[0].keys())
 step_index = 15 #8 #3
-rgb_image = observations[step_index]['rgb'][:, 80:-80]
-depth_image = observations[step_index]['depth'][:, 80:-80]
-
-
+rgb_image = observations[step_index]['rgb']#[:, 80:-80]
+depth_image = observations[step_index]['depth']#[:, 80:-80]
+semantic_image = observations[step_index]['semantic']
 
 visualizer = o3d.visualization.Visualizer()
+
+# ================================ image ==========================
 
 # Add image to visualizer
 visualizer.create_window()
@@ -23,8 +23,10 @@ visualizer.add_geometry(image)
 # Set camera view
 visualizer.get_render_option().background_color = np.asarray([0, 0, 0])
 visualizer.get_view_control().set_zoom(0.5)
-visualizer.run()
+# visualizer.run()
 
+
+# =================================== rgb depth ===================
 
 #reate depth map
 depth_map = o3d.geometry.Image(np.array(depth_image))
@@ -33,7 +35,6 @@ depth_map = o3d.geometry.Image(np.array(depth_image))
 intrinsic = o3d.camera.PinholeCameraIntrinsic(width=640, height=480, fx=500, fy=500, cx=320, cy=240)
 extrinsic = np.eye(4)
 pcd = o3d.geometry.PointCloud.create_from_depth_image(depth_map, intrinsic, extrinsic)
-
 # Create RGBD image from color and depth images
 rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
     o3d.geometry.Image(np.array(rgb_image)),
@@ -41,12 +42,10 @@ rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
 depth_scale=1,
 convert_rgb_to_intensity=False
 )
-
 vis = o3d.visualization.Visualizer()
 vis.create_window()
 vis.add_geometry(rgbd_image)
-import ipdb; ipdb.set_trace()
-vis.run()
+#vis.run()
 
 # Create point cloud from RGBD image
 pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
@@ -63,12 +62,44 @@ point_cloud = torch.from_numpy(points).float()
 vis = o3d.visualization.Visualizer()
 vis.create_window()
 vis.add_geometry(pcd)
+#vis.run()
+
+#===================================== semantic =======================
+import colorsys
+def make_rgb_palette(n=40):
+    HSV_tuples = [(x*1.0/n, 0.8, 0.8) for x in range(n)]
+    RGB_map = np.array(list(map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples)))
+    return RGB_map
+
+semantic_image = semantic_image.squeeze()
+colors = make_rgb_palette(45)
+semantic_colors = colors[semantic_image % 45] * 255
+semantic_colors = semantic_colors.astype(np.uint8)
+
+# Create RGBD image from color and depth images
+semantic_depth_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
+    o3d.geometry.Image(np.array(semantic_colors)),
+    o3d.geometry.Image(np.array(depth_image)),
+depth_scale=1,
+convert_rgb_to_intensity=False
+)
+vis = o3d.visualization.Visualizer()
+vis.create_window()
+vis.add_geometry(semantic_depth_image)
 vis.run()
 
+# Create point cloud from RGBD image
+pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
+    semantic_depth_image,
+    o3d.camera.PinholeCameraIntrinsic(
+        width=640, height=480, fx=500, fy=500, cx=320, cy=240
+    )
+)
 
-
-
-
+vis = o3d.visualization.Visualizer()
+vis.create_window()
+vis.add_geometry(pcd)
+vis.run()
 
 """
 # Extract point cloud data
