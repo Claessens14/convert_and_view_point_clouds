b3ea7560a20630e696534fadfefeacb1295b263f
Jacob Claessens <jacobwclaessens@gmail.com>
2023-03-08 16:53:42 -0500

    code is functional for matterport_category_mappings; trying hm3dsem_category_mappings now

--- img_to_ptcld.py
+++ img_to_ptcld.py
@@ -7,59 +7,59 @@ This file is running with the hab-dev conda env. It's purpuse is to test point c
 for semantic depth images, AND to look at labelling annotations of the file. The .pt file it loads
 has annotations integers with a mean of around 700. What do this corriposond to? This file hopes
 to answer that question, further discover how the meta-labels can be applied, so this dataset
-can be compared to other semantic depth datasets.
+can be compared to other semantic depth datasets. RUN IN TMUX FOR COLOURS
 
 March 6th, 2023, Jacob Claessens
 """
 
 observations = torch.load("train_observations_lst.pt")
-step_index = 3#8 #3 #8 #15 #8 #3
+step_index = 3#8#3#8 #3 #8 #15 #8 #3
 end = len(observations[step_index]['rgb'])
 rgb_image = observations[step_index]['rgb'][:end]#[:, 80:-80]
 depth_image = observations[step_index]['depth'][:end]#[:, 80:-80]
 semantic_image = observations[step_index]['semantic'][:end]
 
 #===================================== semantic =======================
-df = pd.read_csv('matterport_category_mappings.tsv', sep='  +')
-#df.to_excel('data.xlsx', index=False)
-#filtered_df = df[df['raw_category'].str.contains('wall')]
-#print(filtered_df)
-#exit()
-
-
-df['nyuClass'].unique().tolist().index('watch')
-import seaborn as sns
-classes = df['nyuClass'].unique()
-n_colors = len(classes)
-color_palette = sns.color_palette("husl", n_colors)
-palette_rgb = []
-for color in color_palette:
-    color_rgb = tuple(int(x * 255) for x in color)
-    palette_rgb.append(color_rgb) 
-class_color_dict = dict(zip(classes, palette_rgb))
-df['nyuColor'] = df['nyuClass'].map(class_color_dict)
-class_color_index_dict = dict(zip(classes, np.arange(len(classes))))
-df['nyuColorIndex'] = df['nyuClass'].map(class_color_index_dict)
-semantic_image = semantic_image.squeeze()
-def int_to_nyuColor_Rgb(x):
-    point = df['nyuColor'][df['index'] == x].values
-    print(type(point))
-    if len(point) > 0:
-        return np.array(point[0])
-filename = f"sem_img_arr-stepIdx_{step_index}.pt"
-sem_img_arr = None
+tsv_name = "hm3dsem_category_mappings"; df = pd.read_csv(tsv_name+'.tsv', sep='\t')
+#tsv_name = 'matterport_category_mappings'; df = pd.read_csv(tsv_name+'.tsv', sep='  +')
+class_name = 'category'; filename = f"sem_img_arr-stepIdx_{step_index}_{class_name}_{tsv_name}.pt"
+sem_img_arr = None; sem_img_lst = []; class_lst = []
+def add_color_columns():
+    import seaborn as sns
+    classes = df[class_name].unique(); n_colors = len(classes)
+    color_palette = sns.color_palette("husl", n_colors)
+    palette_rgb = []
+    for color in color_palette:
+        color_rgb = tuple(int(x * 255) for x in color)
+        palette_rgb.append(color_rgb) 
+    class_color_dict = dict(zip(classes, palette_rgb))
+    df[class_name + "_color"] = df[class_name].map(class_color_dict)
+    class_color_index_dict = dict(zip(classes, np.arange(len(classes))))
+    df[class_name+'_color_index'] = df[class_name].map(class_color_index_dict)
+add_color_columns()
 if os.path.exists(filename):
-    sem_img_arr = torch.load(filename)
+    obj = torch.load(filename)
+    sem_img_arr = obj['sem_img_arr']
+    class_lst = obj['class_lst']
     print("Model loaded successfully.")
 else:
     print(f"File {filename} not found.")
-    sem_img_lst = []
-    class_lst = []
+    #df.to_excel('data.xlsx', index=False)
+    #filtered_df = df[df['raw_category'].str.contains('wall')]
+    #print(filtered_df)
+    #exit()
+    semantic_image = semantic_image.squeeze()
+   # def int_to_nyuColor_Rgb(x):
+   #     point = df[class_name+'_color'][df['index'] == x].values
+   #     print(type(point))
+   #     if len(point) > 0:
+   #         return np.array(point[0])
     for x in semantic_image:
         sem_img_sub_lst = []
         for y in x:
-            point = df['nyuColor'][df['index'] == y].values
-            class_obj = df['nyuClass'][df['index'] == y].values
+            print(df.head()); exit()
+            point = df[class_name + "_color"][df['index'] == y].values
+            class_obj = df[class_name][df['index'] == y].values
             if len(class_obj) > 0:
                 class_lst.append(class_obj[0])
             if len(point) > 0:
@@ -73,15 +73,15 @@ else:
             import ipdb; ipdb.set_trace()
         sem_img_lst.append(sem_img_sub_arr)
     sem_img_arr = np.array(sem_img_lst, dtype=np.uint8)
-torch.save(sem_img_arr, f"sem_img_arr-stepIdx_{step_index}.pt")
+    torch.save({'sem_img_arr':sem_img_arr, 'class_lst':class_lst}, filename)
 #semantic_point_cloud = np.vectorize(int_to_nyuColor_Rgb)(semantic_image)
 #mask = np.logical_not(np.equal(semeantic_point_cloud, None))
 #semantic_point_cloud = semantic_point_cloud.compress(mask)
-color_legend = {el:df['nyuColor'][df['nyuClass']==el].values[0] for el in class_lst} 
+color_legend = {el:df[class_name + "_color"][df[class_name]==el].values[0] for el in class_lst} 
 print(color_legend)
 def colored_background(r, g, b, text):
     # https://stackoverflow.com/questions/70519979/printing-with-rgb-background
-    return f'\033[48;2;{r};{g};{b}m{text}\033[0m'
+    return f"\033[48;2;{r};{g};{b}m{text}\033[0m"
 for class_name in color_legend:
     rgb = color_legend[class_name]
     print(colored_background(rgb[0], rgb[1], rgb[2], class_name))
@@ -90,16 +90,14 @@ for class_name in color_legend:
 #text = o3d.geometry.Text("Legend", 10)
 #text.paint_uniform_color([1, 1, 1]) # set text color
 # Add point cloud and text to viewport
-import colorsys
-def make_rgb_palette(n=40):
-    HSV_tuples = [(x*1.0/n, 0.8, 0.8) for x in range(n)]
-    RGB_map = np.array(list(map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples)))
-    return RGB_map
-
-
-colors = make_rgb_palette(45)
-semantic_colors = colors[semantic_image % 45] * 255
-semantic_colors = semantic_colors.astype(np.uint8)
+#import colorsys
+#def make_rgb_palette(n=40):
+#    HSV_tuples = [(x*1.0/n, 0.8, 0.8) for x in range(n)]
+#    RGB_map = np.array(list(map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples)))
+#    return RGB_map
+#colors = make_rgb_palette(45)
+#semantic_colors = colors[semantic_image % 45] * 255
+#semantic_colors = semantic_colors.astype(np.uint8)
 
 # Create RGBD image from color and depth images
 semantic_depth_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
